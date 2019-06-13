abb84e17f22bb944796e1453db3314b1494f3deb
hamzakhan <hikshamza>
2019-06-13 15:03:34 +0500

    todo1 Completed

--- src/labTerminalB/Movie.java
+++ src/labTerminalB/Movie.java
@@ -4,6 +4,7 @@ public class Movie {
 	public static final int CHILDRENS = 2;
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
+	public static final int CHILDREN = 0;
 	private String _title;
 	private int _priceCode;
 	public Movie(String title, int priceCode) {
