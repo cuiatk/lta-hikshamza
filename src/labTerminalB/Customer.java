88e4d3f2aad38abdf71bc1e86edf443a47a43058
Adil <Adil@iqra>
2019-06-13 16:02:34 +0500

    Todo4 Completed

--- src/labTerminalB/Customer.java
+++ src/labTerminalB/Customer.java
@@ -49,6 +49,30 @@ public class Customer {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
 			//determine amounts for each line
+			thisAmount = customer(thisAmount, each);
+			// add frequent renter points
+			frequentRenterPoints ++;
+			// add bonus for a two day new release rental
+			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+					&&
+					each.getDaysRented() > 1) frequentRenterPoints ++;
+			//show figures for this rental
+			result += "\t" + each.getMovie().getTitle()+ "\t" +
+					String.valueOf(thisAmount) + "\n";
+			totalAmount += thisAmount;
+		}
+		//add footer lines
+		return addFooter(totalAmount, frequentRenterPoints, result);
+	}
+		private String addFooter(double totalAmount, int frequentRenterPoints, String result) {
+			result += "Amount owed is " + String.valueOf(totalAmount) +
+					"\n";
+			result += "You earned " + String.valueOf(frequentRenterPoints)
+			+
+			" frequent renter points";
+			return result;
+		}
+		private double customer(double thisAmount, Rental each) {
 			switch (each.getMovie().getPriceCode()) {
 			case Movie.REGULAR:
 				thisAmount += 2;
@@ -64,23 +88,6 @@ public class Customer {
 					thisAmount += (each.getDaysRented() - 3) * 1.5;
 				break;
 			}
-			// add frequent renter points
-			frequentRenterPoints ++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&&
-					each.getDaysRented() > 1) frequentRenterPoints ++;
-			//show figures for this rental
-			result += "\t" + each.getMovie().getTitle()+ "\t" +
-					String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+			return thisAmount;
 		}
-		//add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) +
-				"\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
-		+
-		" frequent renter points";
-		return result;
-	}
 }
